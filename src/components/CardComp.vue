858a4ba0e5618ede81264bf3e50c1d1adbbb6cb6
Daniel <angelicodaniel93@gmail.com>
2022-11-29 15:42:13 +0100

    v-if titolo originale

--- src/components/CardComp.vue
+++ src/components/CardComp.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="card">
         <div>{{nomeOggetto.titolo}}</div>
-        <div>{{nomeOggetto.titoloOriginale}}</div>
+        <div v-if="nomeOggetto.titoloOriginale != nomeOggetto.titolo">{{nomeOggetto.titoloOriginale}}</div>
         <div>{{nomeOggetto.lingua}}</div>
         <div>{{nomeOggetto.voto}}</div>
         <img :src="`https://image.tmdb.org/t/p/w342${nomeOggetto.img}`" alt="">
